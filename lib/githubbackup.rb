332427a891b4becc4f77422aabc011d89580b089
Clair <clair_griffiths@hotmail.com>
2016-05-23 15:16:29 +0000

    Working code

--- lib/githubbackup.rb
+++ lib/githubbackup.rb
@@ -5,12 +5,28 @@ class Githubbackup
   def initialize
   end
   
-  def github_backup
-    url = "https://api.github.com/repos/clairgriffiths/Inventory/tarball"
+  def list_repos
+    url = "https://api.github.com/users/clairgriffiths/repos"
     request = HTTParty.get(url)
-    open("hbs-backup-github-#{Date.today}.tar.gz", "wb") do |file|
+    request.each do |repo|
+      github_backup(repo["name"])
+    end
+  end
+  
+  def github_backup(repo_name)
+    url = "https://api.github.com/repos/clairgriffiths/#{repo_name}/tarball"
+    request = HTTParty.get(url)
+    open("temp/github-#{repo_name}.tar.gz", "wb") do |file|
       file.write(request.body)
     end    
   end
   
+  def combine_files
+    `tar zcvf hbs-backup-github-#{Date.today}.tar.gz temp`
+  end
+  
 end
+
+bk = Githubbackup.new
+bk.list_repos
+bk.combine_files
